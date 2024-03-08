32a272a015618d9e2e95aee88ffe649fe8c9d96d
Fran Bec <franbecvort@gmail.com>
2024-03-08 19:09:47 +0000

    main layout components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/animePosterGenerator/mainLayoutComponents/header";
+import Footer from "@/components/animePosterGenerator/mainLayoutComponents/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem> */}
+        <>
+          <main className="flex flex-col h-screen">
+            <div className="flex flex-1 overflow-hidden">
+              <div className="flex flex-1 flex-col">
+                <div className="flex items-center h-16 p-4">
+                  <div className="w-full">
+                    <Header />
+                  </div>
+                </div>
+                <div className="flex flex-1 overflow-y-auto paragraph p-4">
+                  <div className="w-full">{children}</div>
+                </div>
+                <div className="my-2"></div>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="w-full">
+                <Footer />
+              </div>
+            </div>
+          </main>
+        </>
+        {/* </ThemeProvider> */}
+      </body>
     </html>
   );
 }
---
+++ src/components/animePosterGenerator/mainLayoutComponents/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="text-center">
+      Built by <a href="https://pollitodev.netlify.app/">Pollito 🐤</a> with
+      Next.js and ♡
+    </div>
+  )
+}
+
+export default Footer
