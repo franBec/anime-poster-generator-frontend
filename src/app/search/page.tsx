075a073a76c09c698c4b126a8e1dea908c3b4c90
Fran Bec <franbecvort@gmail.com>
2024-03-09 11:55:06 +0000

    /search is able to recognize query param q

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import { SearchAnimeForm } from "@/components/animePosterGenerator/mainPage/searchAnimeForm";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 const SearchPage = () => {
+  const searchParams = useSearchParams();
+  const q = searchParams.get("q");
+
   return (
     <>
       <SearchAnimeForm />
+      <p>{q}</p>
     </>
   );
 };
