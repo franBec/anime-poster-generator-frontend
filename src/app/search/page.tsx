ee9232a9c38b51ef7b80484387e6ca645ad547a4
Fran Bec <franbecvort@gmail.com>
2024-03-10 11:13:12 +0000

    first success search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,15 +3,29 @@
 import { SearchAnimeForm } from "@/components/animePosterGenerator/mainPage/searchAnimeForm";
 import { useSearchParams } from "next/navigation";
 import React from "react";
+import { useGetAnimeSearchQuery } from "../../../generated/rtk-query/jikanApi";
 
 const SearchPage = () => {
   const searchParams = useSearchParams();
-  const q = searchParams.get("q");
+  const q = searchParams.get("q") || "";
+
+  const { data, isLoading, isError } = useGetAnimeSearchQuery(
+    { q },
+    { skip: !q }
+  );
+
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
+
+  if (isError || !data) {
+    return <p>failed to load</p>;
+  }
 
   return (
     <>
       <SearchAnimeForm />
-      <p>{q}</p>
+      <p>{JSON.stringify(data)}</p>
     </>
   );
 };
