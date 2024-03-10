9eacc2ddf9668e54ae756d69afa4b137ab4e7a75
Fran Bec <franbecvort@gmail.com>
2024-03-10 11:30:04 +0000

    script modified so it always makes the files from zero

--- generateRtkQueryCode.ts
+++ generateRtkQueryCode.ts
@@ -1,21 +1,24 @@
 import { existsSync, mkdirSync, writeFileSync } from 'fs';
 import { exec } from 'child_process';
 
+// Define the directory and file path
 const directoryPath = './generated/rtk-query';
 const filePath = `${directoryPath}/jikanApi.ts`;
 
+// Check if the directory exists, if not, create it
 if (!existsSync(directoryPath)) {
     mkdirSync(directoryPath, { recursive: true });
     console.log(`Directory created at: ${directoryPath}`);
 }
 
-if (!existsSync(filePath)) {
-    writeFileSync(filePath, '');
-    console.log(`File created at: ${filePath}`);
-}
+// If the file exists, empty its contents; if not, create it
+writeFileSync(filePath, '');
+console.log(`File contents cleared at: ${filePath}`);
 
+// Define your command
 const command = 'npx @rtk-query/codegen-openapi ./src/schemas/openapi/jikan-config.ts';
 
+// Execute the command
 exec(command, (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
