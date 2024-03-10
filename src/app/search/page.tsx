7a203bc3eafeb5c4a7b588c9cadcafb330a37d4d
Fran Bec <franbecvort@gmail.com>
2024-03-10 12:53:02 +0000

    now showing animes with images

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,16 +6,10 @@ import React from "react";
 import { useGetAnimeSearchQuery } from "../../../generated/rtk-query/jikanApi";
 import Loading from "@/components/animePosterGenerator/layout/loading";
 import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
+import SkeletonWithImage from "@/components/ui/skeleton-with-image";
 
 const SearchPage = () => {
-  const Skeleton = () => (
-    <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-  );
-
-  const trimSynopsis = (
-    synopsis: string | null | undefined,
-    maxLength: number
-  ) => {
+  const trim = (synopsis: string | null | undefined, maxLength: number) => {
     if (!synopsis) return "";
 
     if (synopsis.length > maxLength) {
@@ -41,19 +35,24 @@ const SearchPage = () => {
   }
 
   return (
-    <>
+    <div className="grid gap-4">
       <SearchAnimeForm />
       <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
         {data.data?.map((anime) => (
           <BentoGridItem
             key={anime.mal_id}
-            title={anime.title}
-            description={trimSynopsis(anime.synopsis, 100)}
-            header={<Skeleton />}
+            title={trim(anime.title, 50)}
+            description={trim(anime.synopsis, 150)}
+            header={
+              <SkeletonWithImage
+                src={anime.images?.jpg?.image_url}
+                alt={anime.title}
+              />
+            }
           />
         ))}
       </BentoGrid>
-    </>
+    </div>
   );
 };
 
---
+++ src/components/ui/skeleton-with-image.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+
+const SkeletonWithImage = ({
+  src,
+  alt,
+}: {
+  src: string | null | undefined;
+  alt: string | undefined;
+}) => {
+  if (!src) {
+    return (
+      <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+    );
+  }
+  return (
+    <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl overflow-hidden">
+      <Image
+        src={src}
+        alt={alt || "Content"}
+        width={250}
+        height={250}
+        className="object-cover w-full h-full"
+      />
+    </div>
+  );
+};
+
+export default SkeletonWithImage;
