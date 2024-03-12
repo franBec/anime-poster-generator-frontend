3c8ddd48db500e8967fb310b52f9ef99054adb4f
Fran Bec <franbecvort@gmail.com>
2024-03-12 22:38:50 +0000

    added conditions for showing or not the generate poster button

--- src/components/animePosterGenerator/generatePosterForm.tsx
+++ src/components/animePosterGenerator/generatePosterForm.tsx
@@ -15,8 +15,7 @@ import { imageUrlToBase64 } from "@/lib/imageUrlToBase64";
 import { useState, useEffect } from "react";
 
 const GeneratePosterForm = ({ anime }: { anime: AnimeFull }) => {
-  const [imageBase64, setImageBase64] = useState("");
-
+  const [imageBase64, setImageBase64] = useState<string>();
   useEffect(() => {
     const fetchImageBase64 = async () => {
       const imageUrl = anime.images?.jpg?.large_image_url;
@@ -34,16 +33,7 @@ const GeneratePosterForm = ({ anime }: { anime: AnimeFull }) => {
     }
   }, [anime.images?.jpg?.large_image_url]);
 
-  const form = useForm<MakePosterApiArg>({
-    defaultValues: {
-      posterContent: {
-        title: anime.title,
-        genres: anime.genres?.map((genre) => genre.name),
-        year: anime.year || 0,
-      },
-    },
-  });
-
+  const form = useForm<MakePosterApiArg>();
   const [makePosterAsBlob] = useMakePosterAsBlobMutation();
   const onSubmit: SubmitHandler<MakePosterApiArg> = (makePosterApiArg) => {
     makePosterAsBlob(makePosterApiArg);
@@ -62,64 +52,130 @@ const GeneratePosterForm = ({ anime }: { anime: AnimeFull }) => {
   }
 
   const {
-    data: animeStuff,
+    data: animeStaff,
     isLoading,
     isError,
     error,
   } = useGetAnimeStaffQuery({ id });
 
   if (isLoading) {
-    return <Loading />;
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
   }
 
-  if (isError || !animeStuff) {
+  if (isError || !animeStaff) {
     return (
       <AlertDestructive alertDescription={JSON.stringify(error, null, 2)} />
     );
   }
 
-  if (!animeStuff.data || !animeStuff.data.length) {
+  if (!animeStaff.data || !animeStaff.data.length) {
     return (
       <AlertDestructive
-        alertDescription={`anime/${anime.mal_id} did not brought back any staff information`}
+        alertDescription={`anime/${anime.mal_id} did not bring back any staff information`}
       />
     );
   }
 
   if (!imageBase64) {
-    <AlertDestructive alertDescription={"Not able to get base 64 image"} />;
+    return (
+      <AlertDestructive alertDescription={"Not able to get image for poster"} />
+    );
   }
-
   form.setValue(
     "posterContent.image",
     imageBase64.replace("data:image/jpeg;base64,", "")
   );
-  form.setValue(
-    "posterContent.director",
-    animeStuff.data.find((person) => person.positions?.includes("Director"))
-      ?.person?.name || ""
-  );
-  if (anime.studios) {
-    form.setValue(
-      "posterContent.studios",
-      anime.studios
-        .filter(
-          (studio): studio is { name: string } => studio.name !== undefined
-        )
-        .map((studio) => studio.name) as string[]
+
+  const title = anime.title;
+  if (!title) {
+    return (
+      <AlertDestructive alertDescription={"Not able to get title for poster"} />
     );
   }
-  if (anime.producers) {
-    form.setValue(
-      "posterContent.producers",
-      anime.producers
-        .filter(
-          (producer): producer is { name: string } =>
-            producer.name !== undefined
-        )
-        .map((producer) => producer.name) as string[]
+  form.setValue("posterContent.title", title);
+
+  const year = anime.year;
+  if (!year) {
+    return (
+      <AlertDestructive alertDescription={"Not able to get year for poster"} />
     );
   }
+  form.setValue("posterContent.year", year);
+
+  if (!anime.genres) {
+    return (
+      <AlertDestructive
+        alertDescription={"Not able to get genres for poster"}
+      />
+    );
+  }
+  const genres = anime.genres
+    .filter((genre): genre is { name: string } => genre.name !== undefined)
+    .map((genre) => genre.name) as string[];
+  if (!genres.length) {
+    return (
+      <AlertDestructive
+        alertDescription={"Not able to get genres for poster"}
+      />
+    );
+  }
+  form.setValue("posterContent.genres", genres);
+
+  const director = animeStaff.data.find((person) =>
+    person.positions?.includes("Director")
+  )?.person?.name;
+  if (!director) {
+    return (
+      <AlertDestructive
+        alertDescription={"Not able to get director for poster"}
+      />
+    );
+  }
+  form.setValue("posterContent.director", director);
+
+  if (!anime.studios) {
+    return (
+      <AlertDestructive
+        alertDescription={"Not able to get studios for poster"}
+      />
+    );
+  }
+  const studios = anime.studios
+    .filter((studio): studio is { name: string } => studio.name !== undefined)
+    .map((studio) => studio.name) as string[];
+  form.setValue("posterContent.studios", studios);
+  if (!studios.length) {
+    return (
+      <AlertDestructive
+        alertDescription={"Not able to get studios for poster"}
+      />
+    );
+  }
+
+  if (!anime.producers) {
+    return (
+      <AlertDestructive
+        alertDescription={"Not able to get producers for poster"}
+      />
+    );
+  }
+  const producers = anime.producers
+    .filter(
+      (producer): producer is { name: string } => producer.name !== undefined
+    )
+    .map((producer) => producer.name) as string[];
+  if (!producers.length) {
+    return (
+      <AlertDestructive
+        alertDescription={"Not able to get producers for poster"}
+      />
+    );
+  }
+  form.setValue("posterContent.producers", producers);
 
   return (
     <>
